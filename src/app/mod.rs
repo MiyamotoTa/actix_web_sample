10834a79905fa4e811d007bad1f023f71a1cf6d0
Takahiro Miyamoto <miyamoto_ta@zoho.com>
2020-09-05 15:48:42 +0900

    Use `middleware::NormalizePath`
    Closes #8

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,5 +1,5 @@
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware, App, HttpServer};
 use env_logger::Env;
 
 mod routes;
@@ -8,8 +8,13 @@ mod v1;
 pub(crate) async fn run() -> std::io::Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
 
-    HttpServer::new(|| App::new().wrap(Logger::default()).configure(routes::routes))
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .configure(routes::routes)
+            .wrap(middleware::NormalizePath)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
--- src/app/routes.rs
+++ src/app/routes.rs
@@ -6,9 +6,9 @@ pub(crate) fn routes(app: &mut web::ServiceConfig) {
     app.service(
         web::scope("api").service(
             web::scope("articles")
-                .service(web::resource("").route(web::get().to(articles_handler::index)))
+                .service(web::resource("/").route(web::get().to(articles_handler::index)))
                 .service(
-                    web::resource("{author}/{title}")
+                    web::resource("{author}/{title}/")
                         .route(web::get().to(articles_handler::get_article)),
                 ),
         ),
