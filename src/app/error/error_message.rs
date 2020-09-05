fca292d6ce4e2e775fc1c7f85e6140d9c3cad726
Takahiro Miyamoto <miyamoto_ta@zoho.com>
2020-09-05 23:46:03 +0900

    Add skip serializing.

--- src/app/error/error_message.rs
+++ src/app/error/error_message.rs
@@ -4,5 +4,6 @@ use serde::Serialize;
 pub(crate) struct ErrorMessage {
     pub(crate) status: u16,
     pub(crate) message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) code: Option<u16>,
 }
