220dac64986890126a7fae05caa7a46146edd95e
Takahiro Miyamoto <miyamoto_ta@zoho.com>
2020-09-08 22:39:08 +0900

    Add scope

--- src/app/routes.rs
+++ src/app/routes.rs
@@ -5,15 +5,17 @@ use crate::app::v1::users::handler::users_handler;
 
 pub(crate) fn routes(app: &mut web::ServiceConfig) {
     app.service(
-        web::scope("api")
-            .service(web::scope("users").service(users_handler::create)) // todo scope("users").configure(users_handler::init)
-            .service(
-                web::scope("articles")
-                    .service(web::resource("/").route(web::get().to(articles_handler::index)))
-                    .service(
-                        web::resource("{author}/{title}/")
-                            .route(web::get().to(articles_handler::get_article)),
-                    ),
-            ),
+        web::scope("api").service(
+            web::scope("v1")
+                .service(web::scope("users").service(users_handler::create))
+                .service(
+                    web::scope("articles")
+                        .service(web::resource("/").route(web::get().to(articles_handler::index)))
+                        .service(
+                            web::resource("{author}/{title}/")
+                                .route(web::get().to(articles_handler::get_article)),
+                        ),
+                ),
+        ),
     );
 }
