79797badd92b0260f0632e94c16a258169a31853
Takahiro Miyamoto <miyamoto_ta@zoho.com>
2020-09-12 09:59:28 +0900

    Add validation

--- src/app/v1/users/repository/user_repository.rs
+++ src/app/v1/users/repository/user_repository.rs
@@ -1,19 +1,10 @@
-use crate::app::v1::users::dto::post_users_request::PostUsersRequest;
 use crate::app::v1::users::model::user::User;
 use crate::app::AppState;
 
-pub(crate) async fn create(
-    request: &PostUsersRequest,
-    state: &AppState,
-) -> Result<(), sqlx::Error> {
-    sqlx::query_file!(
-        "queries/v1/users/insert_users.sql",
-        request.name,
-        request.email
-    )
-    .execute(&state.pool)
-    .await?;
-
+pub(crate) async fn create(email: &str, name: &str, state: &AppState) -> Result<(), sqlx::Error> {
+    sqlx::query_file!("queries/v1/users/insert_users.sql", name, email)
+        .execute(&state.pool)
+        .await?;
     Ok(())
 }
 
--- src/app/v1/users/service/users_service.rs
+++ src/app/v1/users/service/users_service.rs
@@ -1,11 +1,25 @@
+use regex::Regex;
+use slog::{error, o};
+
 use crate::app::error::AppError;
-use crate::app::v1::users::dto::post_users_request::PostUsersRequest;
 use crate::app::v1::users::model::user::User;
 use crate::app::v1::users::repository::user_repository;
 use crate::app::AppState;
 
-pub(crate) async fn create(request: &PostUsersRequest, state: &AppState) -> Result<(), AppError> {
-    user_repository::create(request, state)
+pub(crate) async fn create(email: &str, name: &str, state: &AppState) -> Result<(), AppError> {
+    let log = state.log.new(o!("service" => "user_service.create"));
+
+    let email_regex = Regex::new(
+        r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})",
+    )
+    .unwrap();
+    if !email_regex.is_match(email) {
+        let message = "Invalid e-mail format";
+        error!(log, "{}", message);
+        return Err(AppError::un_processable_entity_error(message));
+    }
+
+    user_repository::create(email, name, state)
         .await
         .map_err(AppError::db_error)?;
     Ok(())
